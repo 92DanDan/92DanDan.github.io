367a9ea4f3f8e1254afc70a632166feb8629b232
92DanDan <2505109386@qq.com>
2025-05-01 10:01:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let username = prompt("您的用户名是:");
-  if (!username) username = "匿名用户";
 
-  const greeting = document.getElementById("greeting");
-  greeting.textContent = `您好，${username}！很高兴您来访。`;
-});
